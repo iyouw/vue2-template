0d14770338aad7fd30080851e8ead13a201e1526
staright <wang_xiaobo@hzlh.com>
2022-08-05 08:58:57 +0800

    feat(base): refactor auth

--- src/router/hooks/beforeEach.js
+++ src/router/hooks/beforeEach.js
@@ -1,23 +1,29 @@
 import { userManager } from "@/utils/authentication";
 
-const KEY_LAST_LOCATION = "LAST_ROUTE_PATH";
+const KEY_RETURN_URL_WHEN_SIGNIN_REDIRECT = "RETURN_URL_WHEN_SIGNIN_REDIRECT";
 
 export function yuardBeforeEach(router) {
   router.beforeEach(async (to, from, next) => {
-    // if it is the signin redirect callback
-    if (window.location.search.indexOf("code") !== -1) {
-      try {
-        await userManager.signinRedirectCallback();
-        window.location.href = localStorage.getItem(KEY_LAST_LOCATION);
-      } catch {}
-    } else {
-      const user = await userManager.getUser();
-      if (!user) {
-        localStorage.setItem(KEY_LAST_LOCATION, window.location.href);
-        userManager.signinRedirect();
+    const user = await userManager.getUser();
+    if (!user) {
+      // if it is the signin redirect callback
+      if (window.location.search.indexOf("code") !== -1) {
+        try {
+          await userManager.signinRedirectCallback();
+          const returnUrl = localStorage.getItem(
+            KEY_RETURN_URL_WHEN_SIGNIN_REDIRECT
+          );
+          window.location.replace(returnUrl);
+        } catch {}
       } else {
-        next();
+        localStorage.setItem(
+          KEY_RETURN_URL_WHEN_SIGNIN_REDIRECT,
+          window.location.href
+        );
+        userManager.signinRedirect();
       }
+    } else {
+      next();
     }
   });
 }
